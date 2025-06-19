430a353212e7466d09021dbe20fde55f7b905a27
lifedata2025 <lifedata@tutamail.com>
2025-06-19 23:41:28 +0800

    Update ScheduledCleanup.sh
    垃圾桶定期清空

--- ScheduledCleanup.sh
+++ ScheduledCleanup.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-TARGET_DIR="/path/to/your/dir"
+# 垃圾桶
+TRASH_DIR=~/.trash
 
-LOG_FILE="$HOME/clear_folder.log"
+# 如果不存在垃圾桶
+if [ ! -d "$TRASH_DIR" ]; then
+    mkdir -p "$TRASH_DIR"
+fi
 
-if [ -d "$TARGET_DIR" ]; then
-    # 删除7天前最后修改的文件或目录（不含 TARGET_DIR 本身）
-    find "$TARGET_DIR" -mindepth 1 -mtime +7 -exec rm -rf {} +
+LOG_FILE="$TRASH_DIR/scheduledCleanup.log"
 
-    echo "Deleted files and directories older than 7 days in $TARGET_DIR at $(date)" >> "$LOG_FILE"
+# 需要删除多久（天）以前的文件，根据需求修改
+if [ -d "$TRASH_DIR" ]; then
+    # 删除7天前最后修改的文件
+    find "$TARGET_DIR" -type f -mtime +7 -delete
+    echo "Deleted files older than 7 days in $TARGET_DIR at $(date)" >> "$LOG_FILE"
+    # 删除7天前最后修改的目录
+    find "$TARGET_DIR" -type d -mtime +7 -exec /bin/rm -rf {} +
+    echo "Deleted directories older than 7 days in $TARGET_DIR at $(date)" >> "$LOG_FILE"
 else
-    echo "Directory $TARGET_DIR does not exist at $(date)" >> "$LOG_FILE"
+    echo "Directory $TARGET_DIR does not exist at $(date)" >> "~/err.log"
 fi
