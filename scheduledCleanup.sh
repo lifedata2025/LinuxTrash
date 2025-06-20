66fc82bd3f473e476250300439701f96bb4a86a4
lifedata2025 <lifedata@tutamail.com>
2025-06-20 15:48:46 +0800

    Update scheduledCleanup.sh
    修改变量错误

--- scheduledCleanup.sh
+++ scheduledCleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 垃圾桶
-TRASH_DIR=~/.trash
+TRASH_DIR="$HOME/.trash"
 
 # 如果不存在垃圾桶
 if [ ! -d "$TRASH_DIR" ]; then
@@ -13,11 +13,11 @@ LOG_FILE="$TRASH_DIR/scheduledCleanup.log"
 # 需要删除多久（天）以前的文件，根据需求修改
 if [ -d "$TRASH_DIR" ]; then
     # 删除7天前最后修改的文件
-    find "$TARGET_DIR" -type f -mtime +7 -delete
-    echo "Deleted files older than 7 days in $TARGET_DIR at $(date)" >> "$LOG_FILE"
+    find "$TRASH_DIR" -type f -mtime +7 -delete
+    echo "Deleted files older than 7 days in $TRASH_DIR at $(date)" >> "$LOG_FILE"
     # 删除7天前最后修改的目录
-    find "$TARGET_DIR" -type d -mindepth 1 -mtime +7 -exec /bin/rm -rf {} +
-    echo "Deleted directories older than 7 days in $TARGET_DIR at $(date)" >> "$LOG_FILE"
+    find "$TRASH_DIR" -mindepth 1 -type d -mtime +7 -exec /bin/rm -rf {} +
+    echo "Deleted directories older than 7 days in $TRASH_DIR at $(date)" >> "$LOG_FILE"
 else
-    echo "Directory $TARGET_DIR does not exist at $(date)" >> "~/err.log"
+    echo "Directory $TARGET_DIR does not exist at $(date)" >> "$HOME/err.log"
 fi
